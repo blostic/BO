b05c07c397718722435388150c88dd2c8633c65f
blost <piotr.skibiak@gmail.com>
2013-10-20 20:43:21 +0200

    nic

---
+++ BO/src/bo/project/logic/Closure.java
@@ -0,0 +1,16 @@
+package bo.project.logic;
+
+import java.util.ArrayList;
+
+
+public class Closure {
+	private ArrayList<Road> roads;
+	private double xCoordinate;
+	private double yCoordinate;
+	public double getxCoordinate() {
+		return xCoordinate;
+	}
+	public double getyCoordinate() {
+		return yCoordinate;
+	}
+}
