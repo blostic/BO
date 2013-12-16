af19885f8c326fa251c7f369d8a815892075d71d
Marcin <nmarcin92@gmail.com>
2013-12-16 23:00:01 +0100

    prawdopodobnie ostateczna wersja do debugowania

--- BO/src/bo/project/server/Main.java
+++ BO/src/bo/project/server/Main.java
@@ -2,14 +2,17 @@ package bo.project.server;
 
 import java.io.IOException;
 
+import bo.project.cuckooSwarming.Cuckoo;
+import bo.project.cuckooSwarming.Solution;
 import bo.project.logic.Simulator;
 import bo.project.message.Message;
 import bo.project.message.MessageType;
 
 public class Main {
 	
-	public static final int simulationTimeInterval = 1;
-	public static final int simulationTotalTime = 100;
+	public static final int SIMULATION_TIME_INTERVAL = 1;
+	public static final int SIMULATION_TOTAL_TIME = 100;
+	public static final int MAX_GENERATIONS = 100;
 	
 	public static void main(String[] args) throws IOException {
 		
@@ -19,14 +22,16 @@ public class Main {
 		while (true) {
 			Message request = messageReceiver.getRequest();
 			Simulator simulator = new Simulator(request.getIntersections(), request.getGenerators(),
-								simulationTimeInterval, simulationTotalTime);
+								SIMULATION_TIME_INTERVAL, SIMULATION_TOTAL_TIME);
 			simulator.initializeVehiclesOnRoads();
 			double redLightsArray[] = {30};
 			double greenLightsArray[] = {30};
 			
 			simulator.runSimulation(redLightsArray,greenLightsArray);
 			
-			request.setResult(simulator.getGreenLights(), simulator.getRedLights());
+			Cuckoo cuckoo = new Cuckoo(simulator);
+			Solution solution = cuckoo.cuckooSearch(MAX_GENERATIONS);
+			request.setResult(solution.greenLightsArray, solution.redLightsArray);
 			request.setType(MessageType.SOL_RESPONSE);
 			messageReceiver.addResponse(request);
 		}
