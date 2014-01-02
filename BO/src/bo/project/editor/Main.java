d2aedb4c41fc2a985238fb86feb637b49a1a3c1b
Marcin <nmarcin92@gmail.com>
2014-01-02 20:21:24 +0100

    Look&Feel zmieniony na systemowy

--- BO/src/bo/project/editor/Main.java
+++ BO/src/bo/project/editor/Main.java
@@ -1,12 +1,22 @@
 package bo.project.editor;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class Main {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-			
+		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException
+				| IllegalAccessException | UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
 		TrafficEditorFrame editor = new TrafficEditorFrame();
 			editor.show();
 	}
